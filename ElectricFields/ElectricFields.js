5de9e65645dd6bf85fbef2bee498e34635613f09
zohar <injapanthree@gmail.com>
2022-04-07 17:31:59 -0400

    working on shader

--- ElectricFields/ElectricFields.js
+++ ElectricFields/ElectricFields.js
@@ -1,38 +1,26 @@
 let n = 20;
-let deltaT = 0.00005;
+let deltaT = 0.0005;
 
 let pointCharges = [];
 let f = new Field(8.85418782e-12);
 
-let debugPCPositions = [
-	[740.53033, 242.93774, 0.0],
-	[1014.6804, 308.81152, 0.0],
-	[259.37213, 587.90857, 0.0],
-	[623.41156, 265.09467, 0.0],
-	[825.2103, 637.3064, 0.0],
-	[896.42267, 562.4817, 0.0],
-	[733.23364, 166.10883, 0.0],
-	[73.643005, 698.1762, 0.0],
-	[815.3712, 274.0772, 0.0],
-	[592.60614, 248.419, 0.0],
-	[929.9206, 616.7311, 0.0],
-	[152.48853, 221.82452, 0.0],
-	[998.6251, 469.19, 0.0],
-	[67.17029, 91.1626, 0.0],
-	[71.18628, 125.18701, 0.0],
-	[92.9303, 346.22662, 0.0],
-	[434.65387, 954.375, 0.0],
-	[895.3764, 694.1558, 0.0],
-	[588.9063, 1.9272461, 0.0],
-	[406.01276, 511.8604, 0.0],
-];
+let fieldImg;
+let fieldShader;
+
+function preload() {
+	// load shader
+	fieldShader = loadShader("shaders/field.vert", "shaders/field.frag");
+}
 
 function setup() {
-	createCanvas(1024, 1024);
 	randomSeed(123);
+	pixelDensity(1);
+
+	createCanvas(1024, 1024);
+	fieldImg = createGraphics(1024, 1024, WEBGL);
 
 	for (var i = 0; i < n; i++) {
-		pc = new PointCharge(debugPCPositions[i][0], debugPCPositions[i][1], 1, 1);
+		pc = new PointCharge(random(0, width), random(0, height), 1, 1);
 		pointCharges.push(pc);
 		f.addPointCharge(pc);
 	}
@@ -40,17 +28,38 @@ function setup() {
 
 function draw() {
 	background(0);
-	console.log(frameRate());
+	// console.log(frameRate());
+	// noLoop();
 
 	//show field
-	colorMode(HSB, 2 * PI, 100, 100);
-	for (let i = 0; i < 1024; i++) {
-		for (let j = 0; j < 1024; j++) {
-			fieldVal = f.getFieldAtPosition(createVector(i, j));
-			set(i, j, color(PI - fieldVal.heading(), 100, fieldVal.mag() / 20000));
-		}
-	}
-	updatePixels();
+	// colorMode(HSB, 2 * PI, 100, 100);
+	// for (let i = 0; i < 1024; i++) {
+	// 	for (let j = 0; j < 1024; j++) {
+	// 		fieldVal = f.getFieldAtPosition(createVector(i, j));
+	// 		set(i, j, color(PI - fieldVal.heading(), 100, fieldVal.mag() / 20000));
+	// 	}
+	// }
+	// updatePixels();
+
+	fieldShader.setUniform("u_resolution", [width, width]);
+	fieldShader.setUniform("u_permittivity", 8.85418782e-12);
+	fieldShader.setUniform(
+		"u_xPositions",
+		pointCharges.map((pc) => pc.pos.x / height)
+	);
+	fieldShader.setUniform(
+		"u_yPositions",
+		pointCharges.map((pc) => 1 - pc.pos.y / width)
+	);
+	fieldShader.setUniform(
+		"u_charges",
+		pointCharges.map((pc) => pc.charge)
+	);
+	fieldImg.shader(fieldShader);
+
+	fieldImg.rect(0, 0, width, height);
+
+	image(fieldImg, 0, 0);
 
 	// show points
 	colorMode(RGB, 255);
---
+++ ElectricFields/shaders/field.frag
@@ -0,0 +1,40 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_permittivity;
+uniform float u_xPositions[20];
+uniform float u_yPositions[20];
+uniform float u_charges[20];
+
+const float PI = 3.1415926535;
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main() {
+	float k = 1.0 / (4.0 * 3.1415926535897 * u_permittivity);
+
+	vec2 st = gl_FragCoord.xy/u_resolution.xy;
+
+	vec2 fieldVal = vec2(0, 0);
+
+	for (int i = 0; i < 20; i++) {
+		vec2 r = st - vec2(u_xPositions[i], u_yPositions[i]);
+		float dist = length(r);
+		r = normalize(r);
+		if (dist != 0.0) {
+			fieldVal += r * ((k * u_charges[i]) / (dist * dist));
+		}
+	}
+		
+	vec3 hsbColor = vec3(PI-atan(fieldVal.y/fieldVal.x), 100.0, length(fieldVal) / 20000.0);
+	// vec3 normalizedHsbColor = vec3(hsbColor.r / (2.0 * PI), hsbColor.g / 100.0, hsbColor.b / 100.0);
+	vec3 normalizedHsbColor = vec3(hsbColor.r / (2.0 * PI), hsbColor.g / 100.0, hsbColor.b / 100.0);
+
+	gl_FragColor = vec4(hsv2rgb(normalizedHsbColor), 1.0);
+}
